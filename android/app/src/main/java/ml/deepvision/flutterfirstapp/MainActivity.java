224939c9802ef6395daf3078946abab77e0acdb8
evgkarasev <eugene.karasev@gmail.com>
2020-03-24 14:15:20 +0300

    first commit

---
+++ android/app/src/main/java/ml/deepvision/flutterfirstapp/MainActivity.java
@@ -0,0 +1,6 @@
+package ml.deepvision.flutterfirstapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/kotlin/ml/deepvision/flutter_first_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package ml.deepvision.flutter_first_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
