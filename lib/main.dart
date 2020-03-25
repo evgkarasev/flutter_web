48f7cc469a4b04287a50bbeda0d0a1a87ff99845
evgkarasev <eugene.karasev@gmail.com>
2020-03-25 21:23:57 +0300

    Simple Business Card App v2

--- lib/main.dart
+++ lib/main.dart
@@ -37,56 +37,44 @@ class MyApp extends StatelessWidget {
                   color: Colors.teal.shade50,
                 ),
               ),
-              Container(
+              Card(
                 margin: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
                 color: Colors.white,
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      size: 15,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    size: 25,
+                    color: Colors.teal.shade700,
+                  ),
+                  title: Text(
+                    '+44 123 456 7890',
+                    style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
                       color: Colors.teal.shade700,
                     ),
-                    SizedBox(
-                      width: 30,
-                    ),
-                    Text(
-                      '+44 123 456 7890',
-                      style: TextStyle(
-                        fontFamily: 'SourceSansPro',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15,
-                        color: Colors.teal.shade700,
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ),
-              Container(
+              Card(
                 margin: EdgeInsets.symmetric(horizontal: 20),
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
                 color: Colors.white,
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      size: 15,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 25,
+                    color: Colors.teal.shade700,
+                  ),
+                  title: Text(
+                    'piter.norton@outlook.com',
+                    style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
                       color: Colors.teal.shade700,
                     ),
-                    SizedBox(
-                      width: 30,
-                    ),
-                    Text(
-                      'piter.norton@outlook.com',
-                      style: TextStyle(
-                        fontFamily: 'SourceSansPro',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15,
-                        color: Colors.teal.shade700,
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ),
             ],
