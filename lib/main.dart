261718a2b33f1d9dd05b0e4498472fe15aa05e79
evgkarasev <eugene.karasev@gmail.com>
2020-03-25 20:58:44 +0300

    Simple Business Card App v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,98 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black54,
-        appBar: AppBar(
-          title: Text('My Web App'),
-          backgroundColor: Colors.blueGrey[500],
-        ),
-        body: Center(
-          child: Image(
-            image: NetworkImage(
-                'https://miningzimbabwe.com/wp-content/uploads/2019/01/Alrosa.jpg'),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 40,
+                backgroundImage: AssetImage('images/manmakarov.jpg'),
+              ),
+              Text(
+                'Piter Norton',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 25,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'OUT OF THE BOX THINKER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontWeight: FontWeight.w200,
+                  fontSize: 15,
+                  letterSpacing: 2.5,
+                  color: Colors.teal.shade50,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      size: 15,
+                      color: Colors.teal.shade700,
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Text(
+                      '+44 123 456 7890',
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Colors.teal.shade700,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      size: 15,
+                      color: Colors.teal.shade700,
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Text(
+                      'piter.norton@outlook.com',
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Colors.teal.shade700,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
